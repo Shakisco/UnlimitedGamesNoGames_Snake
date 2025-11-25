b8ecd98797f9adfcb979fb447cff01a62a8d73a3
Matthew Silvestre <131426476+MatthewSilvestre@users.noreply.github.com>
2025-11-25 22:42:39 +0000

    snake.c

--- snake.c
+++ snake.c
@@ -3,13 +3,38 @@
 #include <signal.h>
 #include <unistd.h>
 
-void startup()
+
+void map()
+{
+    clear();
+    for(int i = 0; i <= 40; i++)
+    {
+        mvprintw(0, i + 1, "-");
+        mvprintw(20, i + 1, "-");
+        usleep(80000);
+        refresh();
+    }
+    for(int i = 0; i <= 20; i++)
+    {
+        mvprintw(i, 0, "|");
+        mvprintw(i, 42, "|");
+        usleep(80000);
+        refresh();
+    }
+
+}
+
+void startup(int signum)
 {
     mvprintw(5, 5, "Starting....");
     refresh();
     sleep(3);
+    map();
+
 
 }
+
+
 
 int main(){
 
@@ -19,6 +44,9 @@ int main(){
     signal(SIGALRM, startup);
 
     alarm(1);
+
+    getchar();
+    endwin();
 
     return 0;
 }
